be4c1523e8654ff861e1cdc207cb3a1b25ccab9a
Yash Jariwala <yashjariwala@gmail.com>
2025-03-24 10:41:50 +0530

    Commit to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+//updated by YJ to make it host on github pages
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mpnweb/',
 })
